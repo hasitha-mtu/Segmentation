93ed17468d4683d28632ac236a4860128d5c729f
Hasitha Adikari <adikari.adikari@mycit.ie>
2025-01-02 23:58:35 +0000

    plot model result for comparison

--- models/unet2/train.py
+++ models/unet2/train.py
@@ -6,11 +6,14 @@ import os
 import keras
 from datetime import datetime
 from keras.callbacks import (Callback,
+                             ModelCheckpoint,
                              CSVLogger)
 import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import randint
 
 LOG_DIR = "C:\\Users\AdikariAdikari\PycharmProjects\Segmentation\models\\unet2\logs"
-
+CKPT_DIR = "C:\\Users\AdikariAdikari\PycharmProjects\Segmentation\models\\unet2\ckpt"
 
 def train_model(X_train, y_train, X_val, y_val):
     print(f'X_train shape : {X_train.shape}')
@@ -22,17 +25,19 @@ def train_model(X_train, y_train, X_val, y_val):
         histogram_freq=1
     )
 
+    os.makedirs(CKPT_DIR, exist_ok=True)
     model = unet_model(output_channels=3)
 
     cbs = [
         CSVLogger(LOG_DIR + '/unet_logs.csv', separator=',', append=False),
+        ModelCheckpoint(CKPT_DIR + '/ckpt-{epoch}', save_freq="epoch"),
         tensorboard
     ]
 
     history = model.fit(
                     X_train,
                     y_train,
-                    epochs=500,
+                    epochs=50,
                     batch_size=16,
                     validation_data=(X_val, y_val),
                     callbacks=cbs
@@ -60,6 +65,31 @@ def show_image(image, title=None):
     plt.title(title)
     plt.axis('off')
 
+def load_with_trained_model(X_val, y_val, count=5):
+    checkpoints = [os.path.join(CKPT_DIR, name) for name in os.listdir("ckpt")]
+    print(f"Checkpoints: {checkpoints}")
+    if checkpoints:
+        latest_checkpoint = max(checkpoints, key=os.path.getctime)
+        print(f"Restoring from {latest_checkpoint}")
+        model = keras.models.load_model(latest_checkpoint)
+        for i in range(count):
+            id = randint(len(X_val))
+            image = X_val[id]
+            mask = y_val[id]
+            pred_mask = model.predict(np.expand_dims(image, 0))[0]
+            plt.figure(figsize=(10, 8))
+            plt.subplot(1, 3, 1)
+            show_image(image, title="Original Image")
+            plt.subplot(1, 3, 2)
+            show_image(mask, title="Original Mask")
+            plt.subplot(1, 3, 3)
+            show_image(pred_mask, title="Predicted Mask")
+            plt.tight_layout()
+            plt.show()
+    else:
+        print("No preloaded model")
+    return None
+
 if __name__ == "__main__":
     print(tf.config.list_physical_devices('GPU'))
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -67,3 +97,4 @@ if __name__ == "__main__":
     if len(physical_devices) > 0:
         (X_train, y_train), (X_val, y_val) = load_drone_dataset("../../input/drone/images")
         train_model(X_train, y_train, X_val, y_val)
+        load_with_trained_model(X_val, y_val, count=10)
