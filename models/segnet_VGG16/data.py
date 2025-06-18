ad316172342dc095806946efa963116eb0852e92
Hasitha Adikari <adikari.adikari@mycit.ie>
2025-06-18 21:22:22 +0100

    SegNet model with VGG16 encoder train using 512x512 images

---
+++ models/segnet_VGG16/data.py
@@ -0,0 +1,144 @@
+import numpy as np
+from glob import glob
+from keras.utils import load_img, img_to_array
+import tensorflow as tf
+from tqdm import tqdm
+import random
+import os
+import cv2
+
+
+from models.common_utils.images import load_ndwi_edge_map, selected_channels, format_image, create_confidence_mask
+
+def load_drone_dataset(path, file_extension = "jpg", num_channels=5):
+    total_images = len(os.listdir(path))
+    print(f'total number of images in path is {total_images}')
+    all_image_paths = sorted(glob(path + "/*."+file_extension))
+    random.Random(1337).shuffle(all_image_paths)
+    print(all_image_paths)
+    train_paths = all_image_paths[:300]
+    print(f"train image count : {len(train_paths)}")
+    x_train, y_train = load_drone_images(train_paths, channels=num_channels)
+    test_paths = all_image_paths[300:]
+    print(f"test image count : {len(test_paths)}")
+    x_test, y_test = load_drone_images(test_paths, channels=num_channels)
+    return (x_train, y_train),(x_test, y_test)
+
+def load_drone_images(size, paths, channels):
+    (width, height) = size
+    images = np.zeros(shape=(len(paths), width, height, len(channels)))
+    masks = np.zeros(shape=(len(paths), width, height, 1))
+    for i, path in tqdm(enumerate(paths), total=len(paths), desc="Loading"):
+        image = get_stacked_image(channels, size, path)
+        images[i] = image
+        mask_path = path.replace("images", "annotations")
+        mask_path = mask_path.replace(".jpg", ".png")
+        mask = load_image(size, mask_path, color_mode = "grayscale")
+        masks[i] = mask
+    return images, masks
+
+def load_image(size, path: str, color_mode = "rgb"):
+    img = load_img(path, color_mode=color_mode)
+
+    img_array = img_to_array(img)
+    normalized_img_array = img_array/255.
+    resized_img = tf.image.resize(normalized_img_array, size, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    return resized_img
+
+def get_image(size, path):
+    image_data = load_ndwi_edge_map(path, edge_map_type='sobel')
+    rgb = format_image(size, image_data['rgb'])
+    # print(f'RGB shape: {rgb.shape}')
+    ndwi = format_image(size, image_data['ndwi'])
+    # print(f'NDWI shape: {ndwi.shape}')
+    edges = format_image(size, image_data['edge_map'])
+    # print(f'EDGE map shape: {edges.shape}')
+    stacked_image = np.dstack((rgb, ndwi, edges))
+    # print(f'Stacked image shape: {stacked_image.shape}')
+    return stacked_image
+
+def get_stacked_image(channels, size, path):
+    stacked_image = selected_channels(channels, size, path)
+    return stacked_image
+
+def load_dataset(path, size = (256, 256), file_extension = "JPG",
+                 channels=['RED', 'GREEN', 'BLUE', 'NDWI', 'Canny', 'LBP',
+                           'HSV Saturation', 'HSV Value', 'GradMag', 'Shadow Mask'],
+                 percentage=0.7):
+    total_images = len(os.listdir(path))
+    print(f'total number of images in path is {total_images}')
+    all_image_paths = sorted(glob(path + "/*."+file_extension))
+    random.Random(1337).shuffle(all_image_paths)
+    print(all_image_paths)
+    train_size = int(len(all_image_paths) * percentage)
+    train_paths = all_image_paths[:train_size]
+    print(f"train image count : {len(train_paths)}")
+    x_train, y_train = load_drone_images(size, train_paths, channels=channels)
+    test_paths = all_image_paths[train_size:]
+    print(f"test image count : {len(test_paths)}")
+    x_test, y_test = load_drone_images(size, test_paths, channels=channels)
+    return (x_train, y_train),(x_test, y_test)
+
+
+
+
+if __name__ == "__main__":
+    annotation_dir = "../../input/samples/crookstown/annotations"
+    formatted_annotation_dir = "../../input/samples/segnet/annotations"
+    os.makedirs(formatted_annotation_dir, exist_ok=True)
+    for filename in os.listdir(annotation_dir):
+        path = os.path.join(annotation_dir, filename)
+        print(f"Original mask path: {path}")
+        ann = cv2.imread(path, cv2.IMREAD_GRAYSCALE)  # shape: (H, W)
+
+        mask = create_confidence_mask(ann)
+
+        # Save mask as 8-bit single-channel image
+        updated_mask_path = os.path.join(formatted_annotation_dir, filename)
+        print(f"Updated mask path: {updated_mask_path}")
+        cv2.imwrite(updated_mask_path, (mask * 255).astype(np.uint8))
+
+
+# if __name__ == "__main__":
+#     sample_image = "../../input/samples1/crookstown/images/DJI_20250324094536_0001_V.jpg"
+#     size = (256, 256)
+#     # get_image(size, sample_image)
+#     img = load_image(size, sample_image)
+#     print(f'Image shape: {img.shape}')
+#
+#     sample_mask = "../../input/samples1/crookstown/annotations/DJI_20250324094536_0001_V.png"
+#     size = (256, 256)
+#     # get_image(size, sample_image)
+#     mask = load_image(size, sample_mask, color_mode = "grayscale")
+#     print(f'Mask shape: {mask.shape}')
+#     np.set_printoptions(threshold=sys.maxsize)
+#     print(f'Mask : {mask}')
+#     array_2d = np.squeeze(mask)
+#     # Save to text file
+#     np.savetxt("y_true.txt", array_2d, fmt="%.4f")
+#     generated_mask = (array_2d != 0.0)
+#     np.savetxt("mask.txt", generated_mask, fmt="%.4f")
+#     y_pred_bin = (generated_mask >= 0.5).astype(np.uint8)
+#     np.savetxt("bin_mask.txt", y_pred_bin, fmt="%.4f")
+
+# if __name__ == "__main__":
+#     sample_image = "../../input/samples1/test/images/0001TP_006690.png"
+#     size = (256, 256)
+#     # get_image(size, sample_image)
+#     img = load_image(size, sample_image)
+#     print(f'Image shape: {img.shape}')
+#
+#     sample_mask = "../../input/samples1/test/annotations/0001TP_006690.png"
+#     size = (256, 256)
+#     # get_image(size, sample_image)
+#     mask = load_image(size, sample_mask, color_mode = "grayscale")
+#     print(f'Mask shape: {mask.shape}')
+#     np.set_printoptions(threshold=sys.maxsize)
+#     print(f'Mask : {mask}')
+#     array_2d = np.squeeze(mask)
+#     # Save to text file
+#     np.savetxt("y_true.txt", array_2d, fmt="%.4f")
+#     generated_mask = (array_2d != 0.0)
+#     np.savetxt("mask.txt", generated_mask, fmt="%.4f")
+#     y_pred_bin = (generated_mask >= 0.5).astype(np.uint8)
+#     np.savetxt("bin_mask.txt", y_pred_bin, fmt="%.4f")
--- models/segnet_VGG16/train_model.py
+++ models/segnet_VGG16/train_model.py
@@ -1,24 +1,25 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 import os
 from datetime import datetime
-
-import keras.callbacks_v1
-import matplotlib.pyplot as plt
-import tensorflow as tf
+import keras
+from tensorflow.keras.callbacks import ModelCheckpoint
 from keras.callbacks import (Callback,
                              CSVLogger)
-import numpy as np
-
-from models.segnet.data import load_dataset
-from model import segnet_model, build_rgb_segnet, extend_to_16_channel_model
-from models.common_utils.loss_functions import  recall_m, precision_m, f1_score, masked_dice_loss
-from models.common_utils.accuracy_functions import calculate_accuracy
+import random
+from model import segnet_vgg16
+from loss_function import combined_masked_dice_bce_loss
+from data import load_dataset
 from models.unet_wsl.wsl_utils import show_image
+from models.common_utils.loss_functions import  recall_m, precision_m, f1_score
 
 LOG_DIR = "C:\\Users\AdikariAdikari\PycharmProjects\Segmentation\models\\segnet_VGG16\logs"
 CKPT_DIR = "C:\\Users\AdikariAdikari\PycharmProjects\Segmentation\models\\segnet_VGG16\ckpt"
 OUTPUT_DIR = "C:\\Users\AdikariAdikari\PycharmProjects\Segmentation\models\\segnet_VGG16\output"
 
-def train_model(epoch_count, batch_size, X_train, y_train, X_val, y_val, num_channels, size = (256, 256), restore=True):
+def train_model(epoch_count, batch_size, X_train, y_train, X_val, y_val, width, height,
+                input_channels, restore=True):
     print(f'X_train shape : {X_train.shape}')
     print(f'y_train shape : {y_train.shape}')
 
@@ -30,8 +31,8 @@ def train_model(epoch_count, batch_size, X_train, y_train, X_val, y_val, num_cha
 
     os.makedirs(CKPT_DIR, exist_ok=True)
 
-    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
-        f"{CKPT_DIR}/SegNet_VGG16_best_model.h5",  # or "best_model.keras"
+    model_checkpoint_callback = ModelCheckpoint(
+        f"{CKPT_DIR}/segnet_VGG16_model.h5",  # or "best_model.keras"
         monitor='val_accuracy',
         save_best_only=True,
         save_weights_only=False,  # set to True if you want only weights
@@ -40,8 +41,8 @@ def train_model(epoch_count, batch_size, X_train, y_train, X_val, y_val, num_cha
     )
 
     cbs = [
-        CSVLogger(LOG_DIR+'/segnet_logs.csv', separator=',', append=False),
-        checkpoint_cb,
+        CSVLogger(LOG_DIR+'/segnet_VGG16_logs.csv', separator=',', append=False),
+        model_checkpoint_callback,
         tensorboard
     ]
     # Create a MirroredStrategy.
@@ -49,7 +50,7 @@ def train_model(epoch_count, batch_size, X_train, y_train, X_val, y_val, num_cha
     print("Number of devices: {}".format(strategy.num_replicas_in_sync))
 
     with strategy.scope():
-        model = make_or_restore_model(restore, num_channels, size)
+        model = make_or_restore_model(restore, width, height, input_channels)
 
     history = model.fit(
                     X_train,
@@ -78,180 +79,51 @@ def train_model(epoch_count, batch_size, X_train, y_train, X_val, y_val, num_cha
     plt.show()
     return None
 
-def make_or_restore_model(restore, num_channels, size):
-    (width, height) = size
+def make_or_restore_model(restore, width, height, input_channels):
     if restore:
-        saved_model_path = f"{CKPT_DIR}/SegNet_VGG16_best_model.h5"
+        custom_objects = {'recall_m': recall_m,
+                          'precision_m': precision_m,
+                          'f1_score': f1_score,
+                          'combined_masked_dice_bce_loss': combined_masked_dice_bce_loss}
+        saved_model_path = f"{CKPT_DIR}/segnet_VGG16_model.h5"
         print(f"Restoring from {saved_model_path}")
         return keras.models.load_model(saved_model_path,
-                                    custom_objects={'recall_m': recall_m,
-                                                    'precision_m': precision_m,
-                                                    'f1_score': f1_score,
-                                                    'masked_dice_loss': masked_dice_loss})
+                                       custom_objects = custom_objects,
+                                       compile=True)
     else:
         print("Creating fresh model")
-        return segnet_model(width, height, num_channels)
+        return segnet_vgg16(width, height, input_channels)
 
 def load_with_trained_model(X_val, y_val):
-    saved_model_path = f"{CKPT_DIR}/SegNet_VGG16_best_model.h5"
-    print(f"Restoring from {saved_model_path}")
-    model = keras.models.load_model(saved_model_path,
-                                    custom_objects={'recall_m': recall_m,
-                                                    'precision_m': precision_m,
-                                                    'f1_score': f1_score,
-                                                    'masked_dice_loss': masked_dice_loss})
+    saved_model_path = f"{CKPT_DIR}/segnet_VGG16_model.h5"
+    custom_objects = {'recall_m': recall_m,
+                      'precision_m': precision_m,
+                      'f1_score': f1_score,
+                      'combined_masked_dice_bce_loss': combined_masked_dice_bce_loss}
+    loaded_model = tf.keras.models.load_model(
+        saved_model_path,
+        custom_objects=custom_objects,
+        compile=True
+    )
+    print(f"Model loaded successfully from: {saved_model_path}")
     for i in range(len(X_val)):
+        image_array = X_val[i]
         actual_mask = y_val[i]
-        image = X_val[i]
-        new_image = np.expand_dims(image, axis=0)
-        y_pred = model.predict(new_image)
-        pred_mask = reconstruct_mask(y_pred)
-        calculate_accuracy(actual_mask, y_pred)
-        # pred_class_map = np.argmax(pred_mask, axis=-1)
+
+        input_for_model = np.expand_dims(image_array, axis=0)
+
+        pred_mask = loaded_model.predict(input_for_model)
         plt.figure(figsize=(10, 8))
         plt.subplot(1, 3, 1)
-        rgb_image = image[:, :, :3]
-        show_image(OUTPUT_DIR, rgb_image, index=i, title="Original_Image", save=False)
+        rgb_image = image_array[:, :, :3]
+        show_image(OUTPUT_DIR, rgb_image, index=i, title="Original_Image", save=True)
         plt.subplot(1, 3, 2)
-        show_image(OUTPUT_DIR, actual_mask, index=i, title="Actual_Mask", save=False)
+        show_image(OUTPUT_DIR, actual_mask.squeeze(), index=i, title="Actual_Mask", save=True)
         plt.subplot(1, 3, 3)
-        show_image(OUTPUT_DIR, pred_mask, index=i, title="Predicted_Mask", save=False)
+        show_image(OUTPUT_DIR, pred_mask.squeeze(), index=i, title="Predicted_Mask", save=True)
+
         plt.tight_layout()
         plt.show()
-
-def reconstruct_mask(y_pred):
-    pred_mask = np.argmax(y_pred[0], axis=-1)  # shape: (H, W)
-    # Convert class mask to color
-    colormap = np.array([
-        [0, 0, 0],  # class 0: background
-        [0, 0, 255],  # class 1: water
-    ], dtype=np.uint8)
-    return colormap[pred_mask]
-
-def train_rgb_model(epoch_count, batch_size, X_train, y_train, X_val, y_val, input_shape=(512, 512, 3), num_classes=1, restore=False):
-    print(f'X_train shape : {X_train.shape}')
-    print(f'y_train shape : {y_train.shape}')
-
-    os.makedirs(LOG_DIR, exist_ok=True)
-    tensorboard = keras.callbacks.TensorBoard(
-        log_dir=os.path.join(LOG_DIR, datetime.now().strftime("%Y%m%d-%H%M%S")),
-        histogram_freq=1
-    )
-
-    os.makedirs(CKPT_DIR, exist_ok=True)
-
-    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
-        f"{CKPT_DIR}/SegNet_VGG16_RGB_best_model.h5",  # or "best_model.keras"
-        monitor='val_accuracy',
-        save_best_only=True,
-        save_weights_only=False,  # set to True if you want only weights
-        mode='max',
-        verbose=1
-    )
-
-    cbs = [
-        CSVLogger(LOG_DIR + '/segnet_vgg16_rgb_logs.csv', separator=',', append=False),
-        checkpoint_cb,
-        tensorboard
-    ]
-    # Create a MirroredStrategy.
-    strategy = tf.distribute.MirroredStrategy(cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
-    print("Number of devices: {}".format(strategy.num_replicas_in_sync))
-
-    model = make_or_restore_model_rgb_model(restore, input_shape, num_classes)
-
-    history = model.fit(
-        X_train,
-        y_train,
-        epochs=epoch_count,
-        batch_size=batch_size,
-        validation_data=(X_val, y_val),
-        callbacks=cbs
-    )
-
-    print(f"SegNet-VGG16-RGB Model History: {history.history}")
-
-def make_or_restore_model_rgb_model(restore, input_shape, num_classes):
-    if restore:
-        saved_model_path = f"{CKPT_DIR}/SegNet_VGG16_RGB_best_model.h5"
-        print(f"Restoring from {saved_model_path}")
-        return keras.models.load_model(saved_model_path)
-    else:
-        print("Creating fresh model")
-        return build_rgb_segnet(input_shape=input_shape,
-                                num_classes=num_classes)
-
-def make_or_restore_16_model(restore, input_shape, pretrained_model):
-    if restore:
-        saved_model_path = f"{CKPT_DIR}/SegNet_VGG16_best_model.h5"
-        print(f"Restoring from {saved_model_path}")
-        return keras.models.load_model(saved_model_path,
-                                    custom_objects={'recall_m': recall_m,
-                                                    'precision_m': precision_m,
-                                                    'f1_score': f1_score,
-                                                    'masked_dice_loss': masked_dice_loss})
-    else:
-        print("Creating fresh model")
-        channel_16_model = extend_to_16_channel_model(pretrained_model, input_shape=input_shape)
-        return channel_16_model
-
-def train_extend_to_16_channel_model(epoch_count, batch_size, rgb_model_path, input_shape=(512, 512, 16), restore=False):
-    pretrained_model = keras.models.load_model(rgb_model_path)
-
-    os.makedirs(LOG_DIR, exist_ok=True)
-    tensorboard = keras.callbacks.TensorBoard(
-        log_dir=os.path.join(LOG_DIR, datetime.now().strftime("%Y%m%d-%H%M%S")),
-        histogram_freq=1
-    )
-
-    os.makedirs(CKPT_DIR, exist_ok=True)
-
-    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint(
-        f"{CKPT_DIR}/SegNet_VGG16_best_model.h5",  # or "best_model.keras"
-        monitor='val_accuracy',
-        save_best_only=True,
-        save_weights_only=False,  # set to True if you want only weights
-        mode='max',
-        verbose=1
-    )
-
-    cbs = [
-        CSVLogger(LOG_DIR + '/segnet_vgg16_logs.csv', separator=',', append=False),
-        checkpoint_cb,
-        tensorboard
-    ]
-    # Create a MirroredStrategy.
-    strategy = tf.distribute.MirroredStrategy(cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
-    print("Number of devices: {}".format(strategy.num_replicas_in_sync))
-
-    model = make_or_restore_16_model(restore, input_shape, pretrained_model)
-
-    history = model.fit(
-        X_train,
-        y_train,
-        epochs=epoch_count,
-        batch_size=batch_size,
-        validation_data=(X_val, y_val),
-        callbacks=cbs
-    )
-
-    print(f"SegNet-VGG16-16-Channel Model History: {history.history}")
-
-    accuracy = history.history["accuracy"]
-    val_accuracy = history.history["val_accuracy"]
-    loss = history.history["loss"]
-    val_loss = history.history["val_loss"]
-    epochs = range(1, len(accuracy) + 1)
-    plt.plot(epochs, accuracy, "bo", label="Training accuracy")
-    plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
-    plt.title("Training and validation accuracy")
-    plt.legend()
-    plt.figure()
-    plt.plot(epochs, loss, "bo", label="Training loss")
-    plt.plot(epochs, val_loss, "b", label="Validation loss")
-    plt.title("Training and validation loss")
-    plt.legend()
-    plt.show()
 
 if __name__ == "__main__":
     print(tf.config.list_physical_devices('GPU'))
@@ -259,90 +131,36 @@ if __name__ == "__main__":
     print(f"physical_devices : {physical_devices}")
     print(tf.__version__)
     print(tf.executing_eagerly())
+
+    SEED = 42
+    os.environ['PYTHONHASHSEED'] = str(SEED)
+    tf.random.set_seed(SEED)
+    np.random.seed(SEED)
+    random.seed(SEED)
+    # # Optional: For full reproducibility (if supported by your TF version)
+    # tf.config.experimental.enable_op_determinism()
+
+    image_size = (512, 512) # actual size is (5280, 3956)
+    epochs = 25
+    batch_size = 4
+    channels = ['RED', 'GREEN', 'BLUE']
+    channel_count = len(channels)
     if len(physical_devices) > 0:
-        (RGB_X_train, RGB_y_train), (RGB_X_val, RGB_y_val) = load_dataset("../../input/samples/segnet_512/images",
-                                                          size = (512, 512),
-                                                          file_extension="jpg",
-                                                          channels=['RED', 'GREEN', 'BLUE'],
-                                                          percentage=0.7)
-        train_rgb_model(25, 4, RGB_X_train, RGB_y_train, RGB_X_val, RGB_y_val, input_shape = (512, 512, 3))
         (X_train, y_train), (X_val, y_val) = load_dataset("../../input/samples/segnet_512/images",
-                                                          size=(512, 512),
+                                                          size = image_size,
                                                           file_extension="jpg",
-                                                          channels=['RED', 'GREEN', 'BLUE', 'NDWI', 'Canny', 'LBP',
-                                                                    'HSV Saturation', 'HSV Value', 'GradMag',
-                                                                    'Shadow Mask', 'Lightness', 'GreenRed',
-                                                                    'BlueYellow', 'X', 'Y', 'Z'],
+                                                          channels=channels,
                                                           percentage=0.7)
-        rgb_model_path = f"{CKPT_DIR}/SegNet_VGG16_RGB_best_model.h5"
-        train_extend_to_16_channel_model(25, 4, rgb_model_path, input_shape = (512, 512, 16))
+
+        y_train = (y_train > 0.0).astype(np.float32)
+        y_val = (y_val > 0.0).astype(np.float32)
+        print(f"y_train unique values : {np.unique(y_train)}")
+        print(f"y_val unique values : {np.unique(y_val)}")
+        water_pixels = np.sum(y_val == 1.0)
+        non_water_pixels = np.sum(y_val == 0.0)
+        print(f"Water: {water_pixels}, Non-water: {non_water_pixels}")
+
+        train_model(epochs, batch_size, X_train, y_train, X_val, y_val,
+                    512, 512, 3, restore=False)
+
         load_with_trained_model(X_val, y_val)
-
-# if __name__ == "__main__":
-#     print(tf.config.list_physical_devices('GPU'))
-#     physical_devices = tf.config.experimental.list_physical_devices('GPU')
-#     print(f"physical_devices : {physical_devices}")
-#     print(tf.__version__)
-#     print(tf.executing_eagerly())
-#     image_size = (512, 512) # actual size is (5280, 3956)
-#     epochs = 25
-#     batch_size = 4
-#     channels = ['RED', 'GREEN', 'BLUE', 'NDWI', 'Canny', 'LBP', 'HSV Saturation', 'HSV Value', 'GradMag',
-#                 'Shadow Mask', 'Lightness', 'GreenRed', 'BlueYellow', 'X', 'Y', 'Z']
-#     channel_count = len(channels)
-#     if len(physical_devices) > 0:
-#         (X_train, y_train), (X_val, y_val) = load_dataset("../../input/samples/segnet_512/images",
-#                                                           size = image_size,
-#                                                           file_extension="jpg",
-#                                                           channels=channels,
-#                                                           percentage=0.7)
-#         train_model(epochs, batch_size, X_train, y_train, X_val, y_val, channel_count,
-#                     size = image_size,
-#                     restore=False)
-#         load_with_trained_model(X_val, y_val)
-#
-# if __name__ == "__main__":
-#     print(tf.config.list_physical_devices('GPU'))
-#     physical_devices = tf.config.experimental.list_physical_devices('GPU')
-#     print(f"physical_devices : {physical_devices}")
-#     print(tf.__version__)
-#     print(tf.executing_eagerly())
-#     image_size = (512, 512) # actual size is (5280, 3956)
-#     channels = ['RED', 'GREEN', 'BLUE', 'NDWI', 'Canny', 'LBP', 'HSV Saturation', 'HSV Value', 'GradMag',
-#                 'Shadow Mask', 'Lightness', 'GreenRed', 'BlueYellow', 'X', 'Y', 'Z']
-#     channel_count = len(channels)
-#     if len(physical_devices) > 0:
-#         (X_train, y_train), (X_val, y_val) = load_dataset("../../input/samples1/crookstown/images",
-#                                                           size = image_size,
-#                                                           file_extension="jpg",
-#                                                           channels=channels,
-#                                                           percentage=0.7)
-#         print(f'X_train shape : {X_train.shape}')
-#         print(f'y_train shape : {y_train.shape}')
-#         print(f'X_val shape : {X_val.shape}')
-#         print(f'y_val shape : {y_val.shape}')
-#         load_with_trained_model(X_val, y_val)
-
-# if __name__ == "__main__":
-#     print(tf.config.list_physical_devices('GPU'))
-#     physical_devices = tf.config.experimental.list_physical_devices('GPU')
-#     print(f"physical_devices : {physical_devices}")
-#     print(tf.__version__)
-#     print(tf.executing_eagerly())
-#     image_size = (512, 512) # actual size is (5280, 3956)
-#     epochs = 2
-#     batch_size = 2
-#     channels = ['RED', 'GREEN', 'BLUE', 'NDWI', 'Canny', 'LBP', 'HSV Saturation', 'HSV Value', 'GradMag',
-#                 'Shadow Mask', 'Lightness', 'GreenRed', 'BlueYellow', 'X', 'Y', 'Z']
-#     channel_count = len(channels)
-#     if len(physical_devices) > 0:
-#         (X_train, y_train), (X_val, y_val) = load_dataset("../../input/samples1/crookstown/images",
-#                                                           size = image_size,
-#                                                           file_extension="jpg",
-#                                                           channels=channels,
-#                                                           percentage=0.7)
-#         train_model(epochs, batch_size, X_train, y_train, X_val, y_val, channel_count,
-#                     size=image_size,
-#                     restore=False)
-#         load_with_trained_model(X_val, y_val)
-
