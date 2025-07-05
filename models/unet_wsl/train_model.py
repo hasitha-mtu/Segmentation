acd95c7da2179c73aea5fc5f21e7520087892f87
Hasitha Adikari <adikari.adikari@mycit.ie>
2025-07-05 23:03:21 +0100

    upadte mdoel code

--- models/unet_wsl/train_model.py
+++ models/unet_wsl/train_model.py
@@ -1,9 +1,7 @@
 import os
 from datetime import datetime
 import keras.callbacks_v1
-import matplotlib.pyplot as plt
 import tensorflow as tf
-from h5py.h5i import DATASET
 from keras.callbacks import (Callback,
                              CSVLogger)
 import numpy as np
@@ -11,11 +9,11 @@ import random
 from models.unet_wsl.data import load_dataset
 from models.unet_wsl.model import unet_model
 from models.common_utils.loss_functions import  recall_m, precision_m, f1_score, masked_dice_loss
-from models.common_utils.images import show_image
+from models.common_utils.plot import plot_model_history, plot_prediction
 from tensorflow.keras.callbacks import ModelCheckpoint
 
 MODEL_FILE_NAME = 'unet_wsl_best_model1.h5'
-DATASET_PATH = '../../input/updated_samples/segnet_512/images'
+DATASET_PATH = '../../input/samples/segnet_512/images'
 
 LOG_DIR = "C:\\Users\AdikariAdikari\PycharmProjects\Segmentation\models\\unet_wsl\logs"
 CKPT_DIR = "C:\\Users\AdikariAdikari\PycharmProjects\Segmentation\models\\unet_wsl\ckpt"
@@ -65,23 +63,7 @@ def train_model(epoch_count, batch_size, X_train, y_train, X_val, y_val, num_cha
                     callbacks=cbs
                 )
 
-    print(history.history)
-    accuracy = history.history["accuracy"]
-    val_accuracy = history.history["val_accuracy"]
-    loss = history.history["loss"]
-    val_loss = history.history["val_loss"]
-    epochs = range(1, len(accuracy) + 1)
-    plt.plot(epochs, accuracy, "bo", label="Training accuracy")
-    plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
-    plt.title("Training and validation accuracy")
-    plt.legend()
-    plt.figure()
-    plt.plot(epochs, loss, "bo", label="Training loss")
-    plt.plot(epochs, val_loss, "b", label="Validation loss")
-    plt.title("Training and validation loss")
-    plt.legend()
-    plt.show()
-    return None
+    plot_model_history(history)
 
 def load_saved_model():
     saved_model_path = os.path.join(CKPT_DIR, MODEL_FILE_NAME)
@@ -93,12 +75,6 @@ def load_saved_model():
                                                    'masked_dice_loss': masked_dice_loss},
                                    compile=True)
 
-def make_prediction(image):
-    print(f'make_prediction|image shape:{image.shape}')
-    model  = load_saved_model()
-    pred_mask = model.predict(image)
-    print(f'make_prediction|pred_mask shape:{pred_mask.shape}')
-    return pred_mask
 
 def make_or_restore_model(restore, num_channels, size):
     if restore:
@@ -114,32 +90,15 @@ def load_with_trained_model(X_val, y_val):
         actual_mask = y_val[i]
         formated_image = np.expand_dims(image, 0)
         pred_mask = model.predict(formated_image)
-        print(f'load_with_trained_model|pred_mask shape:{pred_mask.shape}')
-        plt.figure(figsize=(10, 8))
-        plt.subplot(1, 3, 1)
-        rgb_image = image[:, :, :3]
-        show_image(OUTPUT_DIR, rgb_image, index=i, title="Original_Image", save=True)
-        plt.subplot(1, 3, 2)
-        show_image(OUTPUT_DIR, actual_mask.squeeze(), index=i, title="Actual_Mask", save=True)
-        plt.subplot(1, 3, 3)
-        show_image(OUTPUT_DIR, pred_mask.squeeze(), index=i, title="Predicted_Mask", save=True)
+        plot_prediction(i, image[:, :, :3], actual_mask.squeeze(), pred_mask.squeeze(), OUTPUT_DIR)
 
-        plt.tight_layout()
-        plt.show()
-
-
-def plot_attention_weights(channels, weight_lists):
-    result = [sum(values) for values in zip(*weight_lists)]
-    print(f"plot_attention_weights|result: {result}")
-    percentages = [(value/sum(result))*100 for value in result]
-    print(f"plot_attention_weights|percentages: {percentages}")
-    plt.bar(range(1, len(channels) + 1), percentages)
-    plt.xticks(range(1, len(channels) + 1), channels)
-    plt.ylabel("Channel Attention Weight")
-    plt.title("Learned Attention per Channel")
-    plt.tight_layout()
-    plt.show()
-
+def _set_seed(seed):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+    # Optional: For full reproducibility (if supported by your TF version)
+    tf.config.experimental.enable_op_determinism()
 
 if __name__ == "__main__":
     print(tf.config.list_physical_devices('GPU'))
@@ -148,13 +107,7 @@ if __name__ == "__main__":
     print(tf.__version__)
     print(tf.executing_eagerly())
 
-    SEED = 42
-    os.environ['PYTHONHASHSEED'] = str(SEED)
-    tf.random.set_seed(SEED)
-    np.random.seed(SEED)
-    random.seed(SEED)
-    # # Optional: For full reproducibility (if supported by your TF version)
-    # tf.config.experimental.enable_op_determinism()
+    _set_seed(42)
 
     image_size = (512, 512) # actual size is (5280, 3956)
     epochs = 50
@@ -174,27 +127,3 @@ if __name__ == "__main__":
                     size = image_size,
                     restore=False)
         load_with_trained_model(X_val, y_val)
-
-
-# if __name__ == "__main__":
-#     print(tf.config.list_physical_devices('GPU'))
-#     physical_devices = tf.config.experimental.list_physical_devices('GPU')
-#     print(f"physical_devices : {physical_devices}")
-#     print(tf.__version__)
-#     print(tf.executing_eagerly())
-#     image_size = (256, 256) # actual size is (5280, 3956)
-#     epochs = 2
-#     batch_size = 2
-#     channels = ['RED', 'GREEN', 'BLUE', 'NDWI', 'Canny', 'LBP', 'HSV Saturation', 'HSV Value', 'GradMag',
-#                 'Shadow Mask', 'Lightness', 'GreenRed', 'BlueYellow', 'X', 'Y', 'Z']
-#     channel_count = len(channels)
-#     if len(physical_devices) > 0:
-#         (X_train, y_train), (X_val, y_val) = load_dataset("../../input/samples1/crookstown/images",
-#                                                           size = image_size,
-#                                                           file_extension="jpg",
-#                                                           channels=channels,
-#                                                           percentage=0.5)
-#         train_model(epochs, batch_size, X_train, y_train, X_val, y_val, channel_count,
-#                     size = image_size,
-#                     restore=False)
-#         load_with_trained_model(X_val, channels)
