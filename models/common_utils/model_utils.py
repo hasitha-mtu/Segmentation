f51c6438e5026fc7a69b2bf85afd36e5654cbb40
Hasitha Adikari <adikari.adikari@mycit.ie>
2025-08-05 02:08:16 +0100

    generating model save file name

--- models/common_utils/model_utils.py
+++ models/common_utils/model_utils.py
@@ -1,14 +1,21 @@
 import tensorflow_addons as tfa
 import tensorflow as tf
+import os
+from models.common_utils.config import ModelConfig
 
-def get_optimizer(config_val):
+def get_optimizer():
     initial_learning_rate = 1e-4  # A smaller initial learning rate
     weight_decay = 1e-5  # A small, but effective weight decay
-    if config_val == 'AdamW':
+    if ModelConfig.TRAINING_OPTIMIZER == 'AdamW':
         return tfa.optimizers.AdamW(
             weight_decay=weight_decay,
             learning_rate=initial_learning_rate
         )
     else:
         return tf.optimizers.Adam()
+
+def get_model_save_file_name():
+    saved_model_path = os.path.join(ModelConfig.MODEL_SAVE_DIR,
+                                    f'{ModelConfig.SAVED_FILE_NAME}_{ModelConfig.TRAINING_OPTIMIZER}.h5')
+    return saved_model_path
 
