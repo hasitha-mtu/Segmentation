e38204d26b6784b14584efcad5041be73671c368
Hasitha Adikari <adikari.adikari@mycit.ie>
2025-09-15 16:46:45 +0100

    measure time for prediction of  whole catchment update

--- models/river_size.py
+++ models/river_size.py
@@ -1,9 +1,8 @@
 import numpy as np
 import rasterio
-# from models.deeplabv3_plus.train_model import load_saved_model
-from skimage import measure
 from skimage.morphology import convex_hull_image, remove_small_objects
 import cv2
+import time
 
 from models.train import load_saved_unet_model
 from models.train import load_saved_unet_ffc_model
@@ -254,44 +253,65 @@ def predict_for_the_area_given(model, input_image, output_image): ## working
 
 if __name__ == "__main__":
     input_image = '../input/webodm/odm_orthophoto.tif'
+    start_time = time.time()
     unet_model = load_model(load_saved_unet_model, 'AdamW', True)
     output_image = '../output/model_prediction/unet_water_mask.tif'
     predict_for_the_area_given(unet_model, input_image, output_image)
+    print(f'UNET prediction time : {(time.time() - start_time)}')
 
+    start_time = time.time()
     unet_vgg16_model = load_model(load_saved_unet_VGG16_model, 'AdamW', False)
     output_image = '../output/model_prediction/unet_vgg16_water_mask.tif'
     predict_for_the_area_given(unet_vgg16_model, input_image, output_image)
+    print(f'UNET-VGG16 prediction time : {(time.time() - start_time)}')
 
+    start_time = time.time()
     unet_resnet50_model = load_model(load_saved_unet_ResNet50_model, 'AdamW', False)
     output_image = '../output/model_prediction/unet_resnet50_water_mask.tif'
     predict_for_the_area_given(unet_resnet50_model, input_image, output_image)
+    print(f'UNET-ResNet50 prediction time : {(time.time() - start_time)}')
 
+    start_time = time.time()
     unet_plus_plus_model = load_model(load_saved_unet_plus_plus_model, 'Adam', True)
     output_image = '../output/model_prediction/unet_plus_plus_water_mask.tif'
     predict_for_the_area_given(unet_plus_plus_model, input_image, output_image)
+    print(f'UNET++ prediction time : {(time.time() - start_time)}')
 
+    start_time = time.time()
     unet_mobilenetv2_model = load_model(load_saved_unet_MobileNetV2_model, 'Adam', True)
     output_image = '../output/model_prediction/unet_mobilenetv2_water_mask.tif'
     predict_for_the_area_given(unet_mobilenetv2_model, input_image, output_image)
+    print(f'UNET-MobileNetV2 prediction time : {(time.time() - start_time)}')
 
+    start_time = time.time()
     unet_ffc_model = load_model(load_saved_unet_ffc_model, 'Adam', True)
     output_image = '../output/model_prediction/unet_ffc_water_mask.tif'
     predict_for_the_area_given(unet_ffc_model, input_image, output_image)
+    print(f'UNET-FFC prediction time : {(time.time() - start_time)}')
 
+    start_time = time.time()
     segnet_vgg16_model = load_model(load_saved_segnet_VGG16_model, 'AdamW', True)
     output_image = '../output/model_prediction/segnet_vgg16_water_mask.tif'
     predict_for_the_area_given(segnet_vgg16_model, input_image, output_image)
+    print(f'SegNet-VGG16 prediction time : {(time.time() - start_time)}')
 
+    start_time = time.time()
     segnet_model = load_model(load_saved_segnet_model, 'Adam', True)
     output_image = '../output/model_prediction/segnet_water_mask.tif'
     predict_for_the_area_given(segnet_model, input_image, output_image)
+    print(f'SegNet prediction time : {(time.time() - start_time)}')
 
+    start_time = time.time()
     res_unet_plus_plus_model = load_model(load_saved_res_unet_plus_plus_model, 'AdamW', True)
     output_image = '../output/model_prediction/res_unet_plus_pluss_water_mask.tif'
     predict_for_the_area_given(res_unet_plus_plus_model, input_image, output_image)
+    print(f'ResUNET++ prediction time : {(time.time() - start_time)}')
 
+    start_time = time.time()
     deeplabv3_plus_model = load_model(load_saved_deeplabv3_plus_model, 'Adam', True)
     output_image = '../output/model_prediction/deeplabv3_plus_water_mask.tif'
     predict_for_the_area_given(deeplabv3_plus_model, input_image, output_image)
+    print(f'DeepLabV3+ prediction time : {(time.time() - start_time)}')
+
 
 
